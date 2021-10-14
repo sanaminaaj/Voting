6d3edda30d59f9583242bd36ecc596e0195e244a
sanaminaaj <86563339+sanaminaaj@users.noreply.github.com>
2021-10-14 18:49:15 +0530

    Add files via upload

---
+++ Fetch1.php
@@ -0,0 +1,35 @@
+<?php
+    $db_hostname = "127.0.0.1";
+    $db_username = "root";
+    $db_password = "";
+    $db_name = "voting";
+
+    $conn = mysqli_connect($db_hostname, $db_username, $db_password, $db_name);
+    if (!$conn) {
+        echo "Connection failed: " . mysqli_connect_error();
+        exit;
+    }
+
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+
+    $sql = "SELECT * FROM user WHERE username='$username' AND password='$password'";
+
+    $result = mysqli_query($conn, $sql);
+    if (!$result) {
+        echo "Error: " . mysqli_error($conn);
+        exit;
+    }
+
+    $row = mysqli_fetch_assoc($result);
+    if ($row) {
+        echo "Hello " . $row['password'] . "<br/>";
+        ?>
+        <a href="vote.html">click here to vote</a>
+   <?php
+    } else {
+        echo "Login Failed<br/>";
+    }
+
+    mysqli_close($conn);
+?>
